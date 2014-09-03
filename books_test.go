4aae874af7d251ba3ed70f13e56360ca4c18e34e
Christian Stefanescu <st.chris@gmail.com>
2014-09-03 14:38:10 +0200

    More query tests

--- books_test.go
+++ books_test.go
@@ -1,24 +1,48 @@
 package main
 
 import (
+    "code.google.com/p/gosqlite/sqlite"
     "os"
     "testing"
 )
 
+func queryAndExpect(query string, expected int, conn *sqlite.Conn, t *testing.T) {
+    var bookCount = len(getBooks(query, conn))
+    if bookCount != expected {
+        t.Errorf("Expected %v books, found %v", expected, bookCount)
+    }
+}
+
 func TestBasic(t *testing.T) {
     var conn, _ = initDb("./", "test.db")
-    var bookCount = len(getBooks("", conn))
-    if  bookCount != 0 {
-        t.Errorf("Expected 0 books, found %v", bookCount)
-    }
+    defer conn.Close()
+
+    queryAndExpect("", 0, conn, t)
 
     var b = Book{}
     insert(&b, conn)
 
-    bookCount = len(getBooks("", conn))
-    if bookCount != 1 {
-        t.Errorf("Expected 1 books, found %v", bookCount)
-    }
+    queryAndExpect("", 1, conn, t)
+
+    os.Remove("./test.db")
+}
+
+func TestQueries(t *testing.T) {
+    var conn, _ = initDb("./", "test.db")
+    defer conn.Close()
+
+    var b1 = Book{ID:1, Title:"Foo", Author:"bar"}
+    var b2 = Book{ID:2, Title:"Zebra", Author:"bar"}
+    var b3 = Book{ID:3, Title:"Delta Spaces otherchars (subtitle goes here)", Author:"other"}
+    insert(&b1, conn)
+    insert(&b2, conn)
+    insert(&b3, conn)
+
+    queryAndExpect("", 3, conn, t)
+    queryAndExpect("bar", 2, conn, t)
+    queryAndExpect("other", 1, conn, t)
+    queryAndExpect("a", 3, conn, t)
+    queryAndExpect("NO such $tring", 0, conn, t)
 
     os.Remove("./test.db")
 }
