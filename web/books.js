71d97bfc3ce3265bec655a652d173a6cef9c1f5b
Christian Stefanescu <st.chris@gmail.com>
2014-09-05 14:15:56 +0200

    Switched POST to Ajax

---
+++ web/books.js
@@ -0,0 +1,19 @@
+
+$(document).ready(function() {
+    $('#addBookForm')
+        .on('success.form.bv', function(e) {
+            // Prevent form submission
+            e.preventDefault();
+
+            // Get the form instance
+            var $form = $(e.target);
+
+            // Get the BootstrapValidator instance
+            var bv = $form.data('bootstrapValidator');
+
+            // Use Ajax to submit form data
+            $.post($form.attr('action'), $form.serialize(), function(result) {
+                window.location='/';
+            }, 'json');
+        });
+});
